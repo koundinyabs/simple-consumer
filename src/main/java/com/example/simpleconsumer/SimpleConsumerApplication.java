e3c7675745504dfcabc567a84ecbc6aad917d09b
Koundinya Srinivasarao <ksrinivasarao@pivotal.io>
2019-10-24 02:18:49 -0400

    first commit

---
+++ src/main/java/com/example/simpleconsumer/SimpleConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.example.simpleconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleConsumerApplication.class, args);
+	}
+
+}
